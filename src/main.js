f24ecb3ff234c480357fc19e28e95c96d89feb8b
zhen1119869208 <1119869208@qq.com>
2023-06-16 14:57:07 +0800

    axios基础封装

--- src/main.js
+++ src/main.js
@@ -6,6 +6,12 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import { getCategoryAPI } from './apis/text'
+
+getCategoryAPI().then(res=>{
+  console.log(res);
+})
+
 const app = createApp(App)
 
 app.use(createPinia())
