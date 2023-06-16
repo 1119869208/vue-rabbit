9e502f84e9374ba9600a9711498d454a9b398b76
zhen1119869208 <1119869208@qq.com>
2023-06-16 16:38:07 +0800

    静态资源解构搭建

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,8 @@
 <script setup>
-
 </script>
 
 <template>
   <router-view></router-view>
-  <div class="text">text scss</div>
 </template>
 
-<style scoped lang="scss">
-.text {
-  color: $helpColor;
-}
-</style>
+<style scoped lang="scss"></style>
--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,4 +1,11 @@
+<script setup>
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
+</script>
 <template>
-  这是首页
+  <layout-nav></layout-nav>
+  <layout-header></layout-header>
   <router-view></router-view>
+  <layout-footer></layout-footer>
 </template>
