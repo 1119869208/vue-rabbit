a3ff80f1411bf8d624728796423df05ec26e0222
zhen1119869208 <1119869208@qq.com>
2023-06-16 14:27:45 +0800

    element主题配置

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,8 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
+// 导入相对应的包 配置自定义主题颜色
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -17,7 +19,13 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      resolvers: [ElementPlusResolver(
+        {
+          importStyle: "sass",
+          directives: true,
+          version: "2.1.5",
+        }
+      )],
     }),
   ],
   resolve: {
@@ -25,5 +33,16 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `,
+      }
+    }
+  }
 
 })
