e7f8ddc663207e7b484848a3d23ce054a7256af3
zhen1119869208 <1119869208@qq.com>
2023-06-16 16:15:35 +0800

    scss默认色值提交

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,11 @@
 
 <template>
   <router-view></router-view>
+  <div class="text">text scss</div>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+.text {
+  color: $helpColor;
+}
+</style>
