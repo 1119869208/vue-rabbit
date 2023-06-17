3e487497a72ee6403e244b660ef1911bcbb46e03
zhen1119869208 <1119869208@qq.com>
2023-06-17 10:57:26 +0800

    懒加载优化

---
+++ src/directives/index.js
@@ -0,0 +1,23 @@
+import { useIntersectionObserver } from '@vueuse/core'
+export const LazyPlus = {
+  install(app) {
+    app.directive('img-lazy', {
+      mounted(el,binding) {
+        // console.log(el,binding)
+        const { stop } = useIntersectionObserver(
+          // el为标签源于
+          el,
+    
+          // 检测图片是否到了视口高度，到了视口高度自动获取服务器图片链接 isIntersecting 为 布尔值
+          ([{ isIntersecting }]) => {
+            console.log(isIntersecting);
+            if(isIntersecting) {
+              el.src = binding.value
+              stop()
+            }
+          },
+        )
+      }
+    })
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { useIntersectionObserver } from '@vueuse/core'
 
 import App from './App.vue'
 import router from './router'
 
 import '@/styles/common.scss'
+
+// 导入懒加载插件
+import { LazyPlus } from '@/directives/index.js'
 
 // import { getCategoryAPI } from './apis/text'
 
@@ -17,23 +19,8 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(LazyPlus)
 
 app.mount('#app')
 
-app.directive('img-lazy', {
-  mounted(el,binding) {
-    // console.log(el,binding)
-    useIntersectionObserver(
-      // el为标签源于
-      el,
 
-      // 检测图片是否到了视口高度，到了视口高度自动获取服务器图片链接 isIntersecting 为 布尔值
-      ([{ isIntersecting }]) => {
-        // console.log(isIntersecting);
-        if(isIntersecting) {
-          el.src = binding.value
-        }
-      },
-    )
-  }
-})
