c326b45fcd2af9dd05d73357f6feaa1fa10a9b7c
zhen1119869208 <1119869208@qq.com>
2023-06-21 17:50:59 +0800

    切换地址-激活交互实现

--- src/views/Chenkout/index.vue
+++ src/views/Chenkout/index.vue
@@ -20,6 +20,19 @@ onMounted(() => {
 // 控制弹窗开关
 const showDialog = ref(false)
 
+// 弹窗功能实现
+// 点击切换地址的暂存变量
+const cityAddress = ref({})
+const switchAddress = (item) => {
+  cityAddress.value = item
+}
+
+// 弹窗点击确认切换地址
+const config = () => {
+  curAddress.value = cityAddress.value
+  showDialog.value = false
+  cityAddress.value = {}
+}
 
 
 </script>
@@ -125,7 +138,8 @@ const showDialog = ref(false)
   <!-- 切换地址 -->
   <el-dialog v-model="showDialog" title="切换收货地址" width="30%" center>
     <div class="addressWrapper">
-      <div class="text item" v-for="item in checkInfo.userAddresses" :key="item.id">
+      <div class="text item" @click="switchAddress(item)" :class="{ active: item.id === cityAddress.id }"
+        v-for="item in checkInfo.userAddresses" :key="item.id">
         <ul>
           <li><span>收<i />货<i />人：</span>{{ item.receiver }} </li>
           <li><span>联系方式：</span>{{ item.contact }}</li>
@@ -136,7 +150,7 @@ const showDialog = ref(false)
     <template #footer>
       <span class="dialog-footer">
         <el-button>取消</el-button>
-        <el-button type="primary">确定</el-button>
+        <el-button type="primary" @click="config">确定</el-button>
       </span>
     </template>
   </el-dialog>
